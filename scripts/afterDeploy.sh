b9b2977aa7ab3c9f605bfd1421c84cf72ecc85dd
necraidan <benjamin.auzanneau.tm@gmail.com>
2018-11-28 17:37:56 +0100

    Add tooling config

---
+++ scripts/afterDeploy.sh
@@ -0,0 +1,6 @@
+echo "# pkmn build [![Build Status](https://travis-ci.org/necraidan/theMiamBox.svg?branch=dev)](https://travis-ci.org/necraidan/theMiamBox)
+---------
+Built from dev branch.
+" >> dist/README.md
+
+cp dist/index.html dist/404.html
